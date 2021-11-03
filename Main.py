47fbcde2910a59019f3da9e3a9befc0444f049fd
Nastya <semenikhina.a.d@gmail.com>
2021-11-03 22:39:49 +0300

    Итоговое окно с функционалом

--- Main.py
+++ Main.py
@@ -19,15 +19,17 @@ class MyWidget(QMainWindow):
         self.next_button.clicked.connect(self.next)
         self.back_button.clicked.connect(self.back)
 
-
     def run_test(self):
-        #TODO скрыть кнопки
+        self.render_test_count()
+        self.render_buttons()
         test = self.testList[self.n]
         self.c_label.setText(test.getCaption())
         self.q_label.setText(test.getQuestion())
         self.answer_input.setText(test.getAnswer())
-        #TODO отобразить номер теста 1 из 2
 
+    def render_test_count(self):
+        count = str(self.n + 1) + '/' + str(len(self.testList))
+        self.count_label.setText(count)
 
     def next(self):
         test = self.testList[self.n]
@@ -41,6 +43,16 @@ class MyWidget(QMainWindow):
         if self.n != 0:
             self.n -= 1
         self.run_test()
+
+    def render_buttons(self):
+        if self.n < 1:
+            self.back_button.setVisible(False)
+        else:
+            self.back_button.setVisible(True)
+        if self.n + 1 == len(self.testList):
+            self.next_button.setVisible(False)
+        else:
+            self.next_button.setVisible(True)
 
 
 if __name__ == '__main__':

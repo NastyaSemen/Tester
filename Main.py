60c6ba6f92f83cbc4e2ff994dd8f2bc328b8d325
Nastya <semenikhina.a.d@gmail.com>
2021-11-06 20:53:34 +0300

    Итоговое окно с функционалом 1 часть

--- Main.py
+++ Main.py
@@ -2,8 +2,9 @@ import sys
 
 from PyQt5 import uic
 from TestSupplier import TestSupplier
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
+from WarningDialog import WarningDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QPushButton, QLabel
+from PyQt5.QtCore import QPoint
 
 class MyWidget(QMainWindow):
 
@@ -12,14 +13,15 @@ class MyWidget(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Программа тестирования')
         uic.loadUi('main.ui', self)
         self.testList = TestSupplier().getTestList()
         self.run_test()
         self.next_button.clicked.connect(self.next)
         self.back_button.clicked.connect(self.back)
+        self.end_button.setVisible(False)
 
     def run_test(self):
+        self.setWindowTitle('Программа тестирования')
         self.render_test_count()
         self.render_buttons()
         test = self.testList[self.n]
@@ -51,8 +53,27 @@ class MyWidget(QMainWindow):
             self.back_button.setVisible(True)
         if self.n + 1 == len(self.testList):
             self.next_button.setVisible(False)
+            self.next_button.setEnabled(False)
+            self.end_button.setVisible(True)
+            self.end_button.setEnabled(True)
+            self.end_button.setText('Итог')
+            self.end_button.clicked.connect(self.itog)
         else:
             self.next_button.setVisible(True)
+            self.next_button.setEnabled(True)
+            self.end_button.setVisible(False)
+            self.end_button.setEnabled(False)
+
+    def itog(self):
+        coords = QPoint(self.mapToGlobal(QPoint(0,0)))
+        result = self.render_dialog("Название окна", "Вопрос", coords.x(), coords.y())
+
+
+
+    def render_dialog(self, title, answer, x, y):
+        dialog = WarningDialog(title, answer, x, y)
+        result = dialog.render_dialog()
+        print(result)
 
 
 if __name__ == '__main__':

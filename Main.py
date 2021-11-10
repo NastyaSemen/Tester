2f1fe4e262ba74bf369ca6f0061f85d7ee9f9c78
Nastya <semenikhina.a.d@gmail.com>
2021-11-10 21:36:50 +0300

    Итоговое окно с функционалом 2 часть

--- Main.py
+++ Main.py
@@ -3,12 +3,14 @@ import sys
 from PyQt5 import uic
 from TestSupplier import TestSupplier
 from WarningDialog import WarningDialog
+from Diagram import Diagram
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QPushButton, QLabel
 from PyQt5.QtCore import QPoint
 
 class MyWidget(QMainWindow):
 
     testList = []
+    r_answer_count= 0
     n = 0
 
     def __init__(self):
@@ -65,15 +67,71 @@ class MyWidget(QMainWindow):
             self.end_button.setEnabled(False)
 
     def itog(self):
+        test = self.testList[self.n]
+        test.setAnswer(self.answer_input.toPlainText())
         coords = QPoint(self.mapToGlobal(QPoint(0,0)))
         result = self.render_dialog("Название окна", "Вопрос", coords.x(), coords.y())
+        if result:
+            self.prepare_itog_widget()
 
+    def prepare_itog_widget(self):
+        self.clear_widget()
+        self.setWindowTitle('Подведение итога')
+        self.q_label.setText('')
+        for test in self.testList:
+            if test.check():
+                self.r_answer_count += 1
 
+        self.show_diagram_button = QPushButton(self)
+        self.show_diagram_button.setText('Диаграмма')
+        self.show_diagram_button.move(300, 10)
+        self.show_diagram_button.resize(100, 30)
+        self.show_diagram_button.show()
+
+        self.show_diagram_button.clicked.connect(self.show_diagrama)
+
+        self.result_label = QLabel(self)
+        self.result_label.setText('Выполненно правильно - ' + str(self.r_answer_count))
+        self.result_label.resize(200, 30)
+        self.result_label.move(10, 150)
+        self.result_label.show()
+
+        self.end_testing = QPushButton('Завершить тестирование', self)
+        self.end_testing.move(200, 250)
+        self.end_testing.resize(190, 30)
+        self.end_testing.show()
+
+        self.menu_button = QPushButton('Вернуться в меню', self)
+        self.menu_button.move(10, 250)
+        self.menu_button.resize(150, 30)
+        self.menu_button.show()
+
+    def show_diagrama(self):
+        if len(self.testList) == self.r_answer_count:
+            Diagram().show([self.r_answer_count], ['Правильно'])
+        elif self.r_answer_count == 0:
+            Diagram().show([len(self.testList)], ['Неверно'])
+        else:
+            Diagram().show([self.r_answer_count, len(self.testList) - self.r_answer_count])
 
     def render_dialog(self, title, answer, x, y):
         dialog = WarningDialog(title, answer, x, y)
         result = dialog.render_dialog()
-        print(result)
+        return result
+
+    def clear_widget(self):
+        self.next_button.setVisible(False)
+        self.next_button.setEnabled(False)
+        self.end_button.setVisible(False)
+        self.end_button.setEnabled(False)
+        self.count_label.setVisible(False)
+        self.count_label.setEnabled(False)
+        self.answer_input.setVisible(False)
+        self.answer_input.setEnabled(False)
+        self.c_label.setVisible(False)
+        self.c_label.setEnabled(False)
+        self.back_button.setVisible(False)
+        self.back_button.setEnabled(False)
 
 
 if __name__ == '__main__':
